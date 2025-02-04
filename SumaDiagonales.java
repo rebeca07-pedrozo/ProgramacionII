b0ef251265e33b87f81b5a3688b4e6f195a500ed
Rebeca Pedrozo <rebecapedrozocueto@gmail.com>
2025-02-04 05:18:41 -0500

    Sumar diagonales de una matriz

---
+++ SumaDiagonales.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SumaDiagonales {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el tamaño de la matriz: ");
+        int n = scanner.nextInt();
+        int[][] matriz = new int[n][n];
+        int suma = 0;
+
+        System.out.println("Ingrese los elementos de la matriz:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        scanner.close();
+
+        for (int i = 0; i < n; i++) {
+            suma += matriz[i][i];
+            if (i != n - 1 - i) {
+                suma += matriz[i][n - 1 - i];
+            }
+        }
+
+        System.out.println("Suma de las diagonales: " + suma);
+    }
+}
